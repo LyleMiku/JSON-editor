058eacd66d689b6ede83b2b96432bce4e67fe5d5
Lyle Miku <LyleMiku@outlook.com>
2026-03-13 18:46:40 +0800

    Update fileManager.js

--- js/fileManager.js
+++ js/fileManager.js
@@ -2,17 +2,24 @@ function createEmpty(){
 
 editor.setValue("{\n\n}");
 
+setStatus("新建JSON");
+
 }
 
-document.getElementById("upload").addEventListener("change",function(e){
+document.getElementById("upload")
+.addEventListener("change",function(e){
 
 const file = e.target.files[0];
+
+if(!file) return;
 
 const reader = new FileReader();
 
 reader.onload = function(){
 
 editor.setValue(reader.result);
+
+setStatus("已加载: "+file.name);
 
 };
 
@@ -27,28 +34,40 @@ const blob = new Blob(
 {type:"application/json"}
 );
 
-const a=document.createElement("a");
+const a = document.createElement("a");
 
-a.href=URL.createObjectURL(blob);
+a.href = URL.createObjectURL(blob);
 
-a.download="data.json";
+a.download = "data.json";
 
 a.click();
+
+setStatus("已下载JSON");
 
 }
 
 async function saveFile(){
 
-const handle=await window.showSaveFilePicker({
+try{
+
+const handle = await window.showSaveFilePicker({
 
 suggestedName:"data.json"
 
 });
 
-const writable=await handle.createWritable();
+const writable = await handle.createWritable();
 
 await writable.write(editor.getValue());
 
 await writable.close();
+
+setStatus("文件已保存");
+
+}catch(e){
+
+setStatus("保存取消");
+
+}
 
 }
