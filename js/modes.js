707915c799cc21a3abfca8b5082fec89e1ce524d
Lyle Miku <LyleMiku@outlook.com>
2026-03-13 18:50:27 +0800

    Update modes.js

--- js/modes.js
+++ js/modes.js
@@ -1,13 +1,54 @@
-function setMode(mode){
+let editMode = "all"
 
-if(mode==="readonly"){
+let keyRanges = []
 
-editor.updateOptions({readOnly:true});
+function setEditMode(mode){
 
-return;
+editMode = mode
+
+if(mode === "readonly"){
+
+editor.updateOptions({
+readOnly:true
+})
+
+setStatus("只读模式")
+
+return
 
 }
 
-editor.updateOptions({readOnly:false});
+editor.updateOptions({
+readOnly:false
+})
+
+buildKeyIndex()
+
+setStatus("编辑模式: "+mode)
 
 }
+
+editor.onDidChangeModelContent(function(event){
+
+if(editMode !== "value") return
+
+let position = editor.getPosition()
+
+for(let r of keyRanges){
+
+if(
+position.lineNumber >= r.start.lineNumber &&
+position.lineNumber <= r.end.lineNumber
+){
+
+editor.trigger("","undo",null)
+
+setStatus("键名不可修改")
+
+return
+
+}
+
+}
+
+})
